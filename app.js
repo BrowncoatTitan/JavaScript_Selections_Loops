e852cab141758608dbb086c91b7964c9af28a179
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-10-02 11:24:27 -0500

    practiced with loops

--- app.js
+++ app.js
@@ -2,6 +2,63 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for (let x =1; x<=100; x++){
+if (x %2 == 1) {
+    console.log(x);
+}
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (let x =1; x<=100; x++){
+if (x % 3 == 0 && x % 5 == 0){
+    console.log("FIZZBUZZ")
+}
+if (x % 3 == 0){
+    console.log("FIZZ")
+}
+if (x % 5 == 0){
+    console.log("BUZZ")
+}
+}
+//EX 3
+let x =1
+while(x <= 100){
+    if (x %2 == 1) {
+        console.log(x);
+        x += 2;
+    }  
+}
+x = 1
+do{
+    if (x % 2 == 1) {
+        console.log(x);
+        x += 2;
+    }
+} while (x <= 100)
+x=1
+while(x <= 100){
+    if (x % 3 == 0 && x % 5 == 0){
+        console.log("FIZZBUZZ")
+    }
+    if (x % 3 == 0){
+        console.log("FIZZ")
+    }
+    if (x % 5 == 0){
+        console.log("BUZZ")
+    }
+    x++;
+}
+x = 1
+do{
+    if (x % 3 == 0 && x % 5 == 0){
+        console.log("FIZZBUZZ")
+    }
+    if (x % 3 == 0){
+        console.log("FIZZ")
+    }
+    if (x % 5 == 0){
+        console.log("BUZZ")
+    }
+    x++;
+} while(x <= 100)
